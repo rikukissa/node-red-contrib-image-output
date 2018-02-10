823c40e7249191ba278b1fda028f598755554483
Riku Rouvila <riku.rouvila@gmail.com>
2018-02-10 20:47:50 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# 🏞 node-red-contrib-image-output
+
+Simple image output node. Useful for previewing results of face detecting, object recognition etc.
+
+![](./.github/preview.png)
+
+Expects received `msg.payload` to be either a buffer or a base64 string.
+
+
+## Installation
+
+```
+npm install --save node-red-contrib-image-output
+```
---
+++ image-output/image-output.js
@@ -0,0 +1,14 @@
+module.exports = function(RED) {
+  function ImageNode(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+
+    node.on("input", function(msg) {
+      RED.comms.publish("image", {
+        id: this.id,
+        data: msg.payload.toString("base64")
+      });
+    });
+  }
+  RED.nodes.registerType("image-output", ImageNode);
+};
